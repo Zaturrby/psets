19b9c2ccec3c08f0cb701aa9351f94fb6e62789a
Robert-Jan Korteschiel <ubuntu@03f32a75121d>
2016-02-25 18:30:02 +0000

    initials

---
+++ hacker2/initials.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+#include <ctype.h>
+
+// initials.c is a program that asks for a name, and outputs the initials.
+
+int main(void)
+{
+    string name = GetString();
+    int stringLength = strlen(name);
+    
+    for (int i = 0; i < stringLength; i++)
+    {
+        // ignore spaces and output the next alphabetical character as a capital
+        if (isspace(name[i]))
+        {
+            if (isalpha(name[i + 1]))
+            {
+                printf("%c", toupper(name[i + 1]));   
+            }
+        // Cornercase for i = 0
+        } else if ( i == 0 ) 
+        {
+            printf("%c", toupper(name[i])); 
+        }
+    }
+    printf("\n");
+}
